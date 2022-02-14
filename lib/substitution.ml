e8c3c3aeb01d5ca8d57dd8beaa54b26c5b7f9211
0x4287 <38041560+0x4287@users.noreply.github.com>
2022-02-14 11:15:57 +0100

    WIP: Imroved substituiton handling

--- lib/substitution.ml
+++ lib/substitution.ml
@@ -9,8 +9,7 @@ module Log = (val Logs.src_log Logging.substitution_src : Logs.LOG)
 module FormulaId = struct
   module T = struct
     type t =
-      | Invalid of int * Instance.t                     (* ¬(x.τ.vald) *)
-      | Valid of int * Instance.t                       (* x.τ.valid *)
+      | Valid of int * Instance.t * bool                      (* x.τ.valid *)
       | InstEqual of int * Instance.t                   (* x.τ == y.τ *)
       | ValidEqual of int * Instance.t                  (* x.τ.valid == y.τ.valid *)
       | EqExp of Expression.t                           (* x.exp == y.exp *)
@@ -31,8 +30,7 @@ type fid_map =
 let pp_fromula_id (pp : Format.formatter) (fid :FormulaId.t) = 
   let open Fmt in
   match fid with
-    | Invalid (v, i)-> pf pp "invalid_%i.%s" v i.name
-    | Valid (v, i) -> pf pp "valid_%i.%s" v i.name
+    | Valid (v, i, b) -> pf pp "valid_%b_%i.%s" b v i.name
     | InstEqual (v, i) -> pf pp "inst_equal_%i.%s" v i.name
     | ValidEqual (v, i) ->  pf pp "valid_equal_%i.%s" v i.name
     | GtExp e -> pf pp "gt_exp_%a" Pretty.pp_expr_raw e
@@ -88,30 +86,41 @@ let rec get_var expr =
   | BvExpr(Slice(s, _, _)) -> Ok (var_from_sliceable s)
   | BvExpr(Packet(v, _)) -> Ok v
 
-let rec get_subs_expr exp : (Expression.t option) =
+let get_subs_expr exp =
+  (* Log.debug (fun m -> m "@[Finding substitution for %a @]" Pretty.pp_expr_raw exp); *)
   let open Expression in
-  match exp with
-  | ArithExpr (Plus (a1, a2)) -> (
-    let opt1 = get_subs_expr (ArithExpr a1) in
-    match opt1 with
-    | Some _ -> opt1
-    | None ->  get_subs_expr (ArithExpr a2))
-  | ArithExpr (Length (_)) ->  Some exp
-  | ArithExpr (Num (_)) ->  None
-  | BvExpr (Concat (b1, b2)) -> (
-    let opt1 = get_subs_expr (BvExpr b1) in
-    let opt2 = get_subs_expr (BvExpr b2) in
-    match (opt1, opt2) with
-      | Some BvExpr Packet _, Some BvExpr Slice _ -> opt1
-      | Some BvExpr Packet _, Some BvExpr Packet _
-      | Some BvExpr Slice _, Some BvExpr Packet _ -> opt2
-      | Some _ , None ->  opt1
-      | None , Some _ -> opt2
-      | _ -> None)
-  | BvExpr (Slice (_)) -> Some exp
-  | BvExpr (Packet (_)) -> Some exp
-  | BvExpr (Minus (_))
-  | BvExpr (Bv (_)) -> None
+  let rec get_exp e = 
+    match e with
+    | ArithExpr (Plus (a1, a2)) -> (
+      let opt1 = get_exp (ArithExpr a1) in
+      match opt1 with
+      | Some _ -> opt1
+      | None ->  get_exp (ArithExpr a2))
+    | ArithExpr (Length (_)) ->  Some e
+    | ArithExpr (Num (_)) ->  None
+    | BvExpr (Concat (b1, b2)) -> (
+      let opt1 = get_exp (BvExpr b1) in
+      let opt2 = get_exp (BvExpr b2) in
+      match (opt1, opt2) with
+        | Some BvExpr Packet _, Some BvExpr Slice _ -> opt1
+        | Some BvExpr Packet _, Some BvExpr Packet _
+        | Some BvExpr Slice _, Some BvExpr Packet _ -> opt2
+        | Some _ , None ->  opt1
+        | None , Some _ -> opt2
+        | _ -> None)
+    | BvExpr (Slice (_)) -> Some e
+    | BvExpr (Packet (_)) -> Some e
+    | BvExpr (Minus (_))
+    | BvExpr (Bv (_)) -> None
+  in
+  let exp_o = get_exp exp in
+  match exp_o with
+  | Some e -> 
+      (* Log.debug (fun m -> m "@[Found: %a @]" Pretty.pp_expr_raw e);  *)
+      e
+  | None -> 
+      (* Log.debug (fun m -> m "@[Nothing found, returning: %a @]" Pretty.pp_expr_raw exp); *)
+      exp
 
 let replace_expression exp_i exp_o exp_r =
   let open Expression in
@@ -144,20 +153,21 @@ let replace_expression exp_i exp_o exp_r =
 
 
 let extract_to_map form : (FormulaId.t, Formula.t , FormulaId.comparator_witness) Map.t=
+  Log.debug (fun m -> m "====== Building substitution map ======");
   let update_instance m_in i_exp exp = 
     let open FormulaId in
     match i_exp with
     | Sliceable.Instance(var, inst) -> (
       let k_i = EqInst (var, inst) in
-      Log.debug (fun m -> m "@[Searching for %a@]" pp_fromula_id k_i);
+      (* Log.debug (fun m -> m "@[Searching for %a@]" pp_fromula_id k_i); *)
       let inst_opt = Map.find m_in k_i in
       let data =
         match inst_opt with
         | Some inst -> 
-          Log.debug (fun m -> m "@[Found %a@]" Pretty.pp_form_raw inst);
+          (* Log.debug (fun m -> m "@[Found %a@]" Pretty.pp_form_raw inst); *)
           And(inst, exp)
         | None -> 
-          Log.debug (fun m -> m "@[Nothing found@]");
+          (* Log.debug (fun m -> m "@[Nothing found@]"); *)
           exp
         in
         Log.debug (fun m -> m "@[%a: %a@]" pp_fromula_id k_i Pretty.pp_form_raw  data);
@@ -192,7 +202,8 @@ let extract_to_map form : (FormulaId.t, Formula.t , FormulaId.comparator_witness
     | True -> m_in
 
     | Gt (exp1, _) -> (
-      match exp1 with
+      let subs_exp = get_subs_expr exp1 in
+      match subs_exp with
       | ArithExpr _ ->  
         let k = GtExp exp1 in
         Log.debug (fun m -> m "@[%a: %a@]" pp_fromula_id k Pretty.pp_form_raw  f);
@@ -200,30 +211,31 @@ let extract_to_map form : (FormulaId.t, Formula.t , FormulaId.comparator_witness
       | _ -> m_in)
 
     | Eq (exp1, _) -> (
-      match exp1 with
+      let subs_exp = get_subs_expr exp1 in
+      match subs_exp with
       | BvExpr(Slice(inst, hi, lo)) -> (
         let k = EqBvSl (inst, hi, lo) in
         let m_in = update_instance m_in inst f in
         Log.debug (fun m -> m "@[%a: %a@]" pp_fromula_id k Pretty.pp_form_raw  f);
         Map.set m_in ~key:k ~data:f)
       | BvExpr _ ->  
-        let k = EqBv exp1 in
+        let k = EqBv subs_exp in
         Log.debug (fun m -> m "@[%a: %a@]" pp_fromula_id k Pretty.pp_form_raw  f);
         Map.set m_in ~key:k ~data:f
       | ArithExpr _ ->  
-        let k = EqExp exp1 in
+        let k = EqExp subs_exp in
         Log.debug (fun m -> m "@[%a: %a@]" pp_fromula_id k Pretty.pp_form_raw  f);
         Map.set m_in ~key:k ~data:f)
 
       (* x.τ.valid  *)
     | Neg(IsValid(v, i)) -> 
-      let k = Invalid (v, i) in
+      let k = Valid (v, i, false) in
       Log.debug(fun m -> m "@[%a: %a@]" pp_fromula_id k Pretty.pp_form_raw f);
       Map.set m_in ~key:k ~data:f
 
 
     | IsValid(v, i) -> 
-      let k = Valid (v, i) in
+      let k = Valid (v, i, true) in
       Log.debug(fun m -> m "@[%a: %a@]" pp_fromula_id k Pretty.pp_form_raw f);
       Map.set m_in ~key:k ~data:f
     | _ -> 
@@ -269,88 +281,85 @@ let shift_exp_var v_new exp : Expression.t =
   | BvExpr bv -> BvExpr(shift_bv v_new bv)
 
 
-let rec simplify_formula (form : Formula.t) (map : (FormulaId.t , Formula.t , FormulaId.comparator_witness) Map.t) : Formula.t =
-    let result = 
-      match form with
-      | And(f1, f2) -> Ok (And(simplify_formula f1 map, simplify_formula  f2 map))
-      | Or(And(Neg(IsValid(v1, i1)),Neg(IsValid(_))), And(IsValid(_),IsValid(_))) -> 
+let rec simplify_formula form (m_in : (FormulaId.t , Formula.t , FormulaId.comparator_witness) Map.t) : Formula.t =
+  let result = 
+    let open FormulaId in
+    match form with
+    | And(f1, f2) -> Ok (And(simplify_formula f1 m_in, simplify_formula  f2 m_in))
+    | Or(And(Neg(IsValid(v1, i1)),Neg(IsValid(_))), And(IsValid(_),IsValid(_))) -> (
+      let valid_opt = Map.find m_in (Valid(v1, i1, true)) in
+      let invalid_opt = Map.find m_in (Valid(v1, i1, false)) in
+      match  (valid_opt, invalid_opt) with
+      | Some _, None -> 
         let smpl = IsValid(v1, i1) in
-        Log.debug (fun m -> m "@[replaced @ %a @ by@ %a@]" Pretty.pp_form_raw form Pretty.pp_form_raw smpl);
+        Log.debug (fun m -> m "@[--> replaced @ %a @ by@ %a@]" Pretty.pp_form_raw form Pretty.pp_form_raw smpl);
         Ok smpl
-      (* Simplification --> Replace by recursive fun for f1, f2 *)
-      | Or(And(Neg(IsValid(v1, i1)),Neg(IsValid(_))), And(And(IsValid(_),IsValid(_)), And(f1, f2))) -> (
-        let valid_result = Map.find map (Valid i1) in
-        match valid_result with
-        | Some _ -> (
-          let smpl = IsValid(v1, i1) in
-          Log.debug (fun m -> m "@[replaced @ %a @ by@ %a@]" Pretty.pp_form_raw form Pretty.pp_form_raw smpl);
-          Ok (And(smpl, And(f1,f2))))
-        | None -> Ok form)
-      | Or(And(Neg(IsValid(v1, i1)),Neg(IsValid(v2, i2))), And(And(IsValid(v3, i3),IsValid(v4, i4)), Eq (_, _))) -> (
-        let k = (FormulaId.EqInst i1) in
-        let valid_result = Map.find map (Valid i1) in
-        Log.debug (fun m -> m "@[searching for %a@]" pp_fromula_id k);
-        let slice_result = Map.find_or_error map k  in
-        match valid_result with
-        | Some _ -> (
-          let smpl = IsValid(v1, i1) in
-          Log.debug (fun m -> m "@[replaced @ %a @ by@ %a@]" Pretty.pp_form_raw form Pretty.pp_form_raw smpl);
-          match slice_result with
-          | Ok sl -> 
-            Log.debug (fun m -> m "@[appended @ %a@]" Pretty.pp_form_raw sl);
-            Ok (And(smpl, sl))
-          | _ -> Ok smpl)
-        | None -> (
-          match slice_result with
-          | Ok sl -> 
-            Log.debug (fun m -> m "@[appended @ %a@]" Pretty.pp_form_raw sl);
-            Ok (Or(And(Neg(IsValid(v1, i1)),Neg(IsValid(v2, i2))), And(And(IsValid(v3, i3), IsValid(v4, i4)), sl)))
-          | _ -> Ok form))
-      | Eq (exp1 , exp2) -> 
-        let%bind subsId = match exp2 with
-        | ArithExpr Length (_, pkt) -> Ok (FormulaId.EqArith (ArithExpr (Length(0, pkt))))
-        | BvExpr Bv _ -> Ok (EqVal exp2) (*nonsense!?*)
-        | BvExpr Slice (s, h, t) -> (
-          let open Sliceable in
+      | None, Some _ -> 
+        let smpl = Neg(IsValid(v1, i1)) in
+        Log.debug (fun m -> m "@[--> replaced @ %a @ by@ %a@]" Pretty.pp_form_raw form Pretty.pp_form_raw smpl);
+        Ok smpl
+      | Some _, Some _ -> Error (Error.of_string "Valid and Invalid entry found")
+      | _ -> Ok(form))
+      
+    | Or(And(Neg(IsValid(v1, i1)),Neg(IsValid(v2, i2))), And(And(IsValid(v3, i3),IsValid(v4, i4)), f_r)) -> (
+      let f_l_neg = And(Neg(IsValid(v1, i1)),Neg(IsValid(v2, i2))) in
+      let f_l_pos = And(IsValid(v3, i3),IsValid(v4, i4)) in
+      let f_l = Or(f_l_neg, f_l_pos)in
+      let smpl_l =  simplify_formula f_l m_in in
+      let smpl_r = simplify_formula f_r m_in in
+      match smpl_l with
+      | Neg(IsValid(_)) | IsValid(_) -> Ok(And(smpl_l, smpl_r))
+      | _ -> Ok(Or(f_l_neg, And(f_l_pos, smpl_r))))
+    
+    | Eq(exp1, exp2) -> (
+      let subs_id =
+        match exp2 with
+        | ArithExpr Length (_, pkt) -> Some (EqExp(ArithExpr(Length(0, pkt))))
+        | BvExpr Slice(s, hi, lo) -> (
           match s with
-          | Packet (_,p) -> Ok (EqBv (BvExpr (Slice (Packet (0,p), h, t))))
-          | Instance (_, i) -> Ok (EqBv (BvExpr (Slice (Instance (0,i), h, t)))))
-        | BvExpr Packet (_, p) -> Ok (EqBv (BvExpr (Packet (0,p))))
-        | _ -> Error (Error.of_string "Nothing to replace")
-        in
-        let%bind subs = Map.find_or_error map subsId
-        in
+          | Packet (_,p) -> Some (EqBv (BvExpr (Slice (Packet (0,p), hi, lo))))
+          | Instance (_, i) -> Some (EqBv (BvExpr (Slice (Instance (0,i), hi, lo)))))
+        | BvExpr Packet (_, p) -> Some (EqBv (BvExpr (Packet (0,p))))
+        | _ -> None 
+      in
+      match subs_id with
+      | Some id -> (
+        let%bind subs = Core.Map.find_or_error m_in id in
         let%bind substitution_l = 
           match subs with
           | Eq (subs_l, _)
           | Gt (subs_l, _) -> Ok subs_l
           | _ -> Error (Error.of_string "Nothing to replace")
-        in  
+        in 
+        Log.debug (fun m -> m "@[subs_l: %a@]" Pretty.pp_expr_raw substitution_l); 
         let%bind substitution_r = 
           match subs with
           | Eq (_ , subs_r)
           | Gt (_, subs_r) -> Ok subs_r
           | _ -> Error (Error.of_string "Nothing to replace")
         in
+        Log.debug (fun m -> m "@[subs_r: %a@]" Pretty.pp_expr_raw substitution_r); 
         let%bind exp_old = 
-          match subsId with
+          match id with
           | EqBv exp
-          | EqArith exp
-          | EqVal exp -> Ok exp
+          | EqExp exp
+          | GtExp exp -> Ok exp
           | _ -> Error (Error.of_string "Unexpected FormulaId")
         in
-        Log.debug (fun m -> m "@[replaced @ %a @ by@ %a@ in %a@]" 
-          Pretty.pp_expr_raw exp_old 
-          Pretty.pp_expr_raw exp1 
-          Pretty.pp_expr_raw substitution_l);
+        Log.debug (fun m -> m "@[exp_old: %a@]" Pretty.pp_expr_raw exp_old);
         let%bind exp_new = replace_expression substitution_l exp_old exp1
         in
-        Ok (Eq(exp_new, substitution_r))
-      | _ -> Ok form
-    in 
-    match result with 
-    | Ok f -> f
-    | _ -> form
+        Log.debug (fun m -> m "@[--> replaced @ %a @ by@ %a@ in %a@]" 
+        Pretty.pp_expr_raw exp_old 
+        Pretty.pp_expr_raw exp1 
+        Pretty.pp_expr_raw substitution_l);
+        Ok (Eq(exp_new, substitution_r)))
+      | _ -> Ok form)
+    | _ -> Ok form
+  in 
+  match result with
+  | Ok f -> f
+  | _ -> form
 
 
 let rec simplify (hty : HeapType.t) : HeapType.t =
@@ -366,18 +375,17 @@ let rec simplify (hty : HeapType.t) : HeapType.t =
         | _ ->  Error (Error.of_string "subs is not a Refinement")
       in
       match h with
-      |Refinement(s,ht,f) -> (
+      |Refinement(s,ht,f) -> 
+        Log.debug (fun m -> m "====== Simplifying formula ======");
         Ok (Refinement(s, ht, simplify_formula f subs_map))
-      )
-      |Choice(Refinement(s1,ht1,f1), Refinement(s2,ht2,f2)) -> (
+      |Choice(Refinement(s1,ht1,f1), Refinement(s2,ht2,f2)) -> 
         Ok (Choice(Refinement(s1, ht1, simplify_formula f1 subs_map), Refinement(s2, ht2, simplify_formula f2 subs_map)))
-      )
       | _ ->  Error (Error.of_string "h is not a Refinement"))   
     | _ -> Error (Error.of_string "hty is not a substitution"))
   in
   match result with
   | Ok ht -> 
-    Log.debug (fun m -> m "@[Simplifying %a@]" Pretty.pp_header_type_raw hty);
+    Log.debug (fun m -> m "@[Simplfied %a@]" Pretty.pp_header_type_raw hty);
     Log.debug (fun m -> m "@[Resulting hty %a@]" Pretty.pp_header_type_raw ht);
     ht
   | _ -> hty 
